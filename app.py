85e7a1cd32536302f2095bdb1fe9de072ae6b554
StefanK2ff <stefan.k2ff@googlemail.com>
2023-07-13 11:46:31 +0200

    added flask server to return all data under /api/data

--- app.py
+++ app.py
@@ -10,11 +10,23 @@ def load_data(path):
     with open(path, 'r', newline='', encoding='utf-8-sig') as file:
         reader = csv.DictReader(file, delimiter=",")
         for row in reader:
-
             csv_data.append(row)
     return csv_data
 
 
-data = load_data('./data/persons.csv')
+#data = load_data('./data/persons.csv')
 
-print(data)
+#print(data)
+
+# sets up the server
+app = Flask(__name__)
+
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = load_data('./data/persons.csv')
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run()
