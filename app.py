3b8f1312fa19daab1ff887d2765f26ac04ef2eb9
StefanK2ff <stefan.k2ff@googlemail.com>
2023-07-13 11:42:42 +0200

    added function to load csv, rows as dict in list

--- app.py
+++ app.py
@@ -2,3 +2,19 @@ from flask import Flask, jsonify
 import csv
 
 print("Main app, Hello")
+
+
+# loads the data from the CSV file
+def load_data(path):
+    csv_data = []
+    with open(path, 'r', newline='', encoding='utf-8-sig') as file:
+        reader = csv.DictReader(file, delimiter=",")
+        for row in reader:
+
+            csv_data.append(row)
+    return csv_data
+
+
+data = load_data('./data/persons.csv')
+
+print(data)
